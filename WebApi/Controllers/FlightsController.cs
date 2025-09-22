1bc9c6df1de28e9dcfcf5de653469cc53f7a411c
Dima <dimaslizh@gmail.com>
2025-09-22 20:06:51 +0300

    Добавьте файлы проекта.

---
+++ WebApi/Controllers/FlightsController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightsController : ControllerBase
+    {
+        private readonly FlightDbContext _context;
+
+        public FlightsController(FlightDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Flight>>> GetFlights()
+        {
+            return await _context.Flights.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Flight>> CreateFlight(Flight flight)
+        {
+            _context.Flights.Add(flight);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetFlights), new { id = flight.FlightId }, flight);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateFlight(int id, Flight flight)
+        {
+            if (id != flight.FlightId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(flight).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FlightExists(id))
+                {
+                    return NotFound();
+                }
+                throw;
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFlight(int id)
+        {
+            var flight = await _context.Flights.FindAsync(id);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+
+            _context.Flights.Remove(flight);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        private bool FlightExists(int id)
+        {
+            return _context.Flights.Any(e => e.FlightId == id);
+        }
+    }
+}
